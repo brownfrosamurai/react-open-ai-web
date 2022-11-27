484ddc809829f9d4f3e2f07161af12000242c00d
meduna femi <femimeduna@gmail.com>
2022-11-27 00:04:07 -0700

    add media queries for 4K screen

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,7 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div className='gpt3__cta'>
+    <div className='gpt3__cta section__margin'>
       <div className="gpt3__cta-content">
         <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possiblities.</h3>
