30a49f44330deeb6fb3f06115fcc4e3b44ccb5d6
meduna femi <femimeduna@gmail.com>
2022-11-26 22:20:14 -0700

    mobile responsiveness update

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png'
+import dropbox from '../../assets/dropbox.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+import shopify from '../../assets/shopify.png'
+
+export {
+  google,
+  dropbox,
+  slack,
+  atlassian,
+  shopify
+}
